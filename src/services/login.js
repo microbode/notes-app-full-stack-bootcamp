8051e5983bcbad5a9c164b893cc16fa5e9f3d41b
David <bodegas.mail@gmail.com>
2021-09-27 00:32:26 +0200

    Change base url to heroku

--- src/services/login.js
+++ src/services/login.js
@@ -1,5 +1,34 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/api/login";
+// const baseUrl = "http://localhost:3001/api/login";
+const baseUrl = "https://peaceful-lake-47169.herokuapp.com/api/login"
+
+const getAll = () => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
+
+const create = (newObject, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const request = axios.post(baseUrl, newObject, config);
+  return request.then((response) => response.data);
+};
+
+const update = (id, newObject, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const request = axios.put(`${baseUrl}/${id}`, newObject, config);
+  return request.then((response) => response.data);
+};
+
+export default { getAll, create, update };
+
 
 export const login = async (userData, setError) => {
   try {
